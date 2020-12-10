9a1de5452ff0fa03daf5fd13e096bca15bd3a3e0
Tymoteusz Czech <mail@tymek.cz>
2020-12-10 19:16:19 +0100

    dev stack

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const port = 3000
+const port = process.env.PORT || 3000
 const express = require('express')
 var path = require('path')
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+const itemCount = 30
+const data = Array(itemCount).fill(0).map(
+  (_, index) => `box${index}`
+).reduce(
+  (items, item) => {
+    items[item] = false
+    return items
+  },
+  {}
+)
+
+app.use('/get', (req, res) => {
+  res.json(data)
+})
+
+app.listen(port)
